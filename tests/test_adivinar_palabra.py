8b71b5879cac5222dd7ab152efdde510dd0c59ab
agus32 <agusluzzini@gmail.com>
2024-06-03 22:04:49 -0300

    completados los dest adivinar_palabra y ingresar_nombre

--- tests/test_adivinar_palabra.py
+++ tests/test_adivinar_palabra.py
@@ -13,3 +13,8 @@ def test_palabra_correcta_case_sensitive():
 def test_palabra_correcta_case_sensitive_invertido():
     assert adivinar_palabra("hola","hOlA") == True
 
+def test_palabra_correcta_con_espacios():
+    assert adivinar_palabra("    h ol  a   ","hola") == True
+
+def test_palabra_correcta_con_numeros():
+    assert adivinar_palabra("hola1","hola") == False
--- tests/test_ingresar_nombre.py
+++ tests/test_ingresar_nombre.py
@@ -7,23 +7,32 @@ def test_nombre_vacio():
 def test_nombre_correcto():
     assert verificar_nombre("Juan") == True
 
-def test_nombre_con_espacios():
-    assert verificar_nombre("   Ana   ") == True
+def test_nombre_con_espacios_a_los_costados():
+    assert verificar_nombre("   Ana   ") == False
+
+def test_nombre_con_espacios_entre_letras():
+    assert verificar_nombre("J u a n") == True
 
 def test_nombre_con_caracteres_especiales():
-    assert verificar_nombre("M@ría") == False
+    assert verificar_nombre("M@r.a") == False
+
+def test_nombre_con_tilde():
+    assert verificar_nombre("María") == True
 
 def test_nombre_con_numeros():
     assert verificar_nombre("Pedro123") == True
 
 def test_nombre_con_simbolos():
-    assert verificar_nombre("!@#$%") == True
+    assert verificar_nombre("!@#$%") == False
 
-def test_nombre_con_longitud_maxima():
-    assert verificar_nombre("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") == True
+def test_nombre_con_longitud_maxima(): #20 caracteres
+    assert verificar_nombre("abcdefghijklmnopqrst") == True
+
+def test_nombre_con_longitud_minima():
+    assert verificar_nombre("ab") == False
 
 def test_nombre_con_longitud_excedida():
-    assert verificar_nombre("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1") == False
+    assert verificar_nombre("abcdefghijklmnopqrstu") == False
 
 
 
