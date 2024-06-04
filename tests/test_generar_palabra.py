65d11c5e2205375035bf43d40c56fb5010b11e64
agus32 <agusluzzini@gmail.com>
2024-06-03 22:10:24 -0300

    arreglado generar palabra

--- tests/test_generar_palabra.py
+++ tests/test_generar_palabra.py
@@ -2,37 +2,37 @@ from '../main' import generar_palabra
 import pytest
 
 def test_palabra_vacio():
-    assert verificar_nombre("") == False
+    assert generar_palabra("") == False
 
 def test_palabra_con_mayuscula():
-    assert verificar_nombre("Juan") == False
+    assert generar_palabra("Juan") == False
 
 def test_palabra_con_espacios():
-    assert verificar_nombre("   Ana   ") == False
+    assert generar_palabra("   Ana   ") == False
 
 def test_palabra_con_caracteres_especiales():
-    assert verificar_nombre("M@ría") == False
+    assert generar_palabra("M@ría") == False
 
 def test_palabra_con_numeros():
-    assert verificar_nombre("Pedro123") == False
+    assert generar_palabra("Pedro123") == False
 
 def test_palabra_con_simbolos():
-    assert verificar_nombre("!@#$%") == False
+    assert generar_palabra("!@#$%") == False
 
 def test_palabra_con_longitud_maxima():
-    assert verificar_nombre("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") == False
+    assert generar_palabra("abcdefghijklmnopqrstuvwxyz") == True
 
 def test_palabra_con_longitud_excedida():
-    assert verificar_nombre("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1") == False
+    assert generar_palabra("abcdefghijklmnopqrstuvwxyzA") == False
 
 def test_palabra_con_longitud_minima():
-    assert verificar_nombre("pa") == False
+    assert generar_palabra("pa") == False
 
 def test_palabra_correcta():
-    assert verificar_nombre("palangana") == True
+    assert generar_palabra("palangana") == True
 
 def test_nombre_con_tilde():
-    assert verificar_nombre("ramíro") == false
+    assert generar_palabra("ramíro") == False
 
 def test_palabra_mas_palabras():
-    assert verificar_nombre("martin genaro") == false
+    assert generar_palabra("martin genaro") == False
